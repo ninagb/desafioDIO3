1f89ec7d497eed531ef526165c55d369976f84c8
Marina <marinagbran@gmail.com>
2025-03-21 13:18:39 -0300

    Exercício de classe/ objeto

---
+++ index.js
@@ -0,0 +1,49 @@
+/*Crie uma classe generica que represente um herói de uma aventura e que possua a propriedade tipo (ex: guerreiro, mago, monge, ninja):
+
+além disso, deve ter um método chamado atacar que deve atender os seguintes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenado ao tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada */
+
+class heroi {
+  constructor(tipo) {
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    if (this.tipo === "mago") {
+      ataque = "magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo === "monge") {
+      ataque = "artes marciais";
+    } else if (this.tipo === "ninja") {
+      ataque = "shuriken";
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}.`);
+  }
+}
+
+let heroiMago = new heroi("mago");
+let heroiGuerreiro = new heroi("guerreiro");
+let heroiMonge = new heroi("monge");
+let heroiNinja = new heroi("ninja");
+heroiMago.atacar();
+heroiGuerreiro.atacar();
+heroiMonge.atacar();
+heroiNinja.atacar();
